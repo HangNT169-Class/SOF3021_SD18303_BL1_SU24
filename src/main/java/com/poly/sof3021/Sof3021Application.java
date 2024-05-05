50655f4ac626d6ca86f28cfa827ef4ca5f17aa17
HangNT <HangNT169@fpt.edu.vn>
2024-05-05 21:18:17 +0700

    Initial commit

---
+++ src/main/java/com/poly/sof3021/Sof3021Application.java
@@ -0,0 +1,13 @@
+package com.poly.sof3021;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sof3021Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Sof3021Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/poly/sof3021/Sof3021ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.poly.sof3021;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sof3021ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
